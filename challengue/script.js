f1d6f252976c68d15335565c3550f9835cb0763f
SilviaCamacho2030 <157410427+SilviaCamacho2030@users.noreply.github.com>
2024-02-16 08:30:41 -0500

    Proyecto final primer Challenge de Alura Latam y Oracle ONE
    Este es el archivo final con 6 carpetas donde se encuentra todo el código, incluyendo imágenes del challenge numero uno de Alura Latam y Oracle ONE, el challenge tiene por nombre "Encriptador de texto"

---
+++ challengue/script.js
@@ -0,0 +1,27 @@
+function encriptar() {
+  var frase = document.getElementById("textoEncriptado").value.toLowerCase();
+  var textoEncriptado = frase.replace(/e/gim, "enter");
+  var textoEncriptado = textoEncriptado.replace(/i/gim, "imes");
+  var textoEncriptado = textoEncriptado.replace(/a/gim, "ai");
+  var textoEncriptado = textoEncriptado.replace(/o/gim, "ober");
+  var textoEncriptado = textoEncriptado.replace(/u/gim, "ufat");
+  document.getElementById("textoDesencriptado").innerHTML = textoEncriptado;
+  document.getElementById("botonCopiar").style.display = "show";
+  document.getElementById("botonCopiar").style.display = "inherit";
+}
+
+function desencriptar() {
+  var frase = document.getElementById("textoEncriptado").value.toLowerCase();
+  var textoEncriptado = frase.replace(/enter/gim, "e");
+  var textoEncriptado = textoEncriptado.replace(/imes/gim, "i");
+  var textoEncriptado = textoEncriptado.replace(/ai/gim, "a");
+  var textoEncriptado = textoEncriptado.replace(/ober/gim, "o");
+  var textoEncriptado = textoEncriptado.replace(/ufat/gim, "u");
+  document.getElementById("textoDesencriptado").innerHTML = textoEncriptado;
+}
+
+function copiar() {
+  var contenido = document.querySelector("#textoDesencriptado");
+  contenido.select();
+  document.execCommand("copy");
+}
